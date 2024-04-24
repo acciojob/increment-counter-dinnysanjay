07c1a81409f2b21e1ce4759235ace9c323ffb2b0
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-04-24 19:28:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let body=document.getElementById("my_body");
+function incrementBtn(){
+	let elm=document.createElement("div");
+	elem.innerHTML+="<P>Callhouse:300 says</p>";
+	console.log(elem);
+	body.appendchild(elem);
+}
